378605661d91bd73dd90444deef12013891fa82a
Arjun Maudhoo <apm526@nyu.edu>
2022-10-04 18:14:39 -0400

    Fix the round playing and game functions
    Case in which the player inputs invalid options, still breaks code

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var options = [
-    "Rock",
-    "Paper",
-    "Scissors"
-];
+let computerScore = 0
+let playerScore = 0
 
 function getComputerChoice() {
+    var options = [
+        "Rock",
+        "Paper",
+        "Scissors"
+    ];
     let choice = options[Math.floor(Math.random()*options.length)];
     return choice.toLowerCase();
 }
@@ -25,22 +27,52 @@ function getPlayerChoice() {
         return ("not an option");
     }
 }
-function playRound(getComputerChoice, getPlayerChoice) {
+
+function playRound() {
     let winner = ""
-    if (getComputerChoice == getPlayerChoice) {
-        winner = "tie"
+    const computerChoice = getComputerChoice();
+    const playerChoice = getPlayerChoice();
+    console.log("The Computer picked " + computerChoice);
+    console.log("Player picked " + playerChoice);
+    if (computerChoice == playerChoice) {
+        winner = "Tie, try again";
+        return winner;
     }
     else if 
     (
-        (getPlayerChoice == "rock" && getComputerChoice == "scissors") ||
-        (getPlayerChoice == "paper" && getComputerChoice == "rock") ||
-        (getPlayerChoice == "scissors" && getComputerChoice == "paper")
+        (playerChoice == "rock" && computerChoice == "scissors") ||
+        (playerChoice == "paper" && computerChoice == "rock") ||
+        (playerChoice == "scissors" && computerChoice == "paper")
     ) {
-        winner = "player";
+        winner = "Player wins this round";
+        playerScore ++
+        return winner;
     }
     else {
-        winner = "computer";
+        winner = "Computer wins this round";
+        computerScore ++
+        return winner;
     }
     console.log(winner);
 }
 
+function playGame(){
+        playRound();
+        if (playerScore == 3) {
+            console.log("Player Score: " + playerScore);
+            console.log("Computer Score: " + computerScore);
+            console.log("Player wins the game!");
+            return;
+        }
+        else if (computerScore == 3) {
+            console.log("Player Score: " + playerScore);
+            console.log("Computer Score: " + computerScore);
+            console.log("Computer wins the game!");
+            return;
+        }
+        else {
+            console.log("Player Score: " + playerScore);
+            console.log("Computer Score: " + computerScore);
+            playGame();
+        }
+}
